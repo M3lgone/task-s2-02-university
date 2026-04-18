47a0653613fb84c64d3934540b07b07c3d2ec9b1
M3lgone <ismaelgn89@gmail.com>
2026-04-18 14:19:35 +0200

    Update queries 6 to 20.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -26,50 +26,108 @@ FROM asignatura
 WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
 
 -- 6. Retorna un llistat dels professors/es juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom. (apellido1, apellido2, nombre, departamento)
-
+SELECT p.apellido1, p.apellido2, p.nombre, d.nombre AS departamento
+FROM profesor pr
+JOIN persona p 
+ON pr.id_profesor = p.id
+JOIN departamento d 
+ON pr.id_departamento = d.id
+ORDER BY p.apellido1 ASC, p.apellido2 ASC, p.nombre ASC;
 
 -- 7. Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne/a amb NIF 26902806M. (nombre, anyo_inicio, anyo_fin)
-
+SELECT a.nombre, c.anyo_inicio, c.anyo_fin
+FROM persona p
+JOIN alumno_se_matricula_asignatura m ON p.id = m.id_alumno
+JOIN asignatura a ON m.id_asignatura = a.id
+JOIN curso_escolar c ON m.id_curso_escolar = c.id
+WHERE p.nif = '26902806M';
 
 -- 8. Retorna un llistat amb el nom de tots els departaments que tenen professors/es que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015). (nombre)
-
+SELECT DISTINCT d.nombre
+FROM departamento d
+JOIN profesor pr ON pr.id_departamento = d.id
+JOIN asignatura a ON a.id_profesor = pr.id_profesor
+JOIN grado g ON a.id_grado = g.id
+WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
 
 -- 9. Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019. (nombre, apellido1, apellido2)
-
+SELECT DISTINCT p.nombre, p.apellido1, p.apellido2
+FROM persona p
+JOIN alumno_se_matricula_asignatura a ON a.id_alumno = p.id
+JOIN curso_escolar c ON a.id_curso_escolar = c.id
+WHERE p.tipo = 'alumno' AND c.anyo_inicio = 2018 AND c.anyo_fin = 2019;
 
 -- Resol les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN.
 -- 10. Retorna un llistat amb els noms de tots els professors/es i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors/es que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor/a. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom. (departamento, apellido1, apellido2, nombre)
-
+SELECT d.nombre AS departamento, p.apellido1, p.apellido2, p.nombre
+FROM persona p
+RIGHT JOIN profesor pr ON p.id = pr.id_profesor
+LEFT JOIN departamento d ON pr.id_departamento = d.id
+WHERE p.tipo = 'profesor'
+ORDER BY d.nombre, p.apellido1, p.apellido2, p.nombre;
 
 -- 11. Retorna un llistat amb els professors/es que no estan associats a un departament. (apellido1, apellido2, nombre)
-
+SELECT p.apellido1, p.apellido2, p.nombre
+FROM persona p
+RIGHT JOIN profesor pr ON p.id = pr.id_profesor
+LEFT JOIN departamento d ON pr.id_departamento = d.id
+WHERE p.tipo = 'profesor' AND d.id IS NULL;
 
 -- 12. Retorna un llistat amb els departaments que no tenen professors/es associats. (nombre)
-
+SELECT d.nombre
+FROM departamento d
+LEFT JOIN profesor pr ON pr.id_departamento = d.id
+WHERE pr.id_profesor IS NULL;
 
 -- 13. Retorna un llistat amb els professors/es que no imparteixen cap assignatura. (apellido1, apellido2, nombre)
-
+SELECT p.apellido1, p.apellido2, p.nombre
+FROM persona p
+RIGHT JOIN profesor pr ON p.id = pr.id_profesor
+LEFT JOIN asignatura a ON a.id_profesor = pr.id_profesor
+WHERE p.tipo = 'profesor' AND a.id IS NULL;
 
 -- 14. Retorna un llistat amb les assignatures que no tenen un professor/a assignat. (id, nombre)
-
+SELECT a.id, a.nombre
+FROM asignatura a 
+LEFT JOIN profesor pr ON a.id_profesor = pr.id_profesor
+WHERE pr.id_profesor IS NULL;
 
 -- 15. Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar. (nombre)
-
+SELECT d.nombre
+FROM departamento d
+RIGHT JOIN profesor pr ON pr.id_departamento = d.id
+LEFT JOIN asignatura a ON a.id_profesor = pr.id_profesor
+WHERE a.id IS NULL;
 
 -- 16. Retorna el nombre total d'alumnes que hi ha. (total)
-
+SELECT COUNT(p.nombre) AS total
+FROM persona p
+WHERE tipo = 'alumno';
 
 -- 17. Calcula quants alumnes van néixer en 1999. (total)
-
+SELECT  COUNT(p.nombre) AS total
+FROM persona p 
+WHERE tipo = 'alumno' AND YEAR(p.fecha_nacimiento) = 1999;
 
 -- 18. Calcula quants professors/es hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. El resultat només ha d'incloure els departaments que tenen professors/es associats i haurà d'estar ordenat de major a menor pel nombre de professors/es. (departamento, total)
-
+SELECT d.nombre AS departamento, COUNT(pr.id_profesor) AS total
+FROM profesor pr
+JOIN departamento d ON pr.id_departamento = d.id
+GROUP BY d.nombre
+ORDER BY total DESC;
 
 -- 19. Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d'ells. Tingui en compte que poden existir departaments que no tenen professors/es associats. Aquests departaments també han d'aparèixer en el llistat. (departamento, total)
-
+SELECT d.nombre AS departamento, COUNT(pr.id_profesor) AS total
+FROM departamento d
+LEFT JOIN profesor pr ON pr.id_departamento = d.id
+GROUP BY d.nombre;
 
 -- 20. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingues en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures. (grau, total)
-
+SELECT g.nombre AS grau, COUNT(a.id) AS total
+FROM grado g 
+LEFT JOIN asignatura a ON a.id_grado = g.id
+GROUP BY g.nombre
+ORDER BY total DESC;
 
 -- 21. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades. (grau, total)
 
